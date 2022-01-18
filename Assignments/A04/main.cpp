e0311ba45fb94acf4a8ae64ebc3c88ffb7d52991
Tranvex <89526959+tranvex@users.noreply.github.com>
2022-01-18 12:05:57 -0500

    Update main.cpp

--- Assignments/A04/main.cpp
+++ Assignments/A04/main.cpp
@@ -1,22 +1,118 @@
+/*****************************************************************************
+*                    
+*  Author:           Yoseph Helal
+*  Email:            tranvex@gmail.com
+*  Label:            A04
+*  Title:            Linked List Class
+*  Course:           CMPS 3013
+*  Semester:         Spring 2022
+* 
+*  Description:
+*        This program implements a class that allows a linked list to be used just like 
+*        an array. It overloads the "[]" (square brackets) to simulate accessing seperate 
+*        array elements, but really it traverses the list to find the specified node using
+*        an index value. It also overloads the "+" and "-" signs allowing a user to "add"
+*        or "push" items onto the end of the list, as well as "pop" items off the end of our 
+*        array. This class is not meant to replace the STL vector library, its simply a project
+*        to introduce the beginnings of creating complex / abstract data types. 
+*        
+*  Usage: 
+*       - $ ./main filename
+*       - This will read in a file containing whatever values to be read into our list/array. 
+*       
+*  Files:            
+*       main.cpp    : driver program 
+*****************************************************************************/
+
 #include <iostream>
 
 using namespace std;
 
 int A[100];
 
+/**
+ * Node
+ * 
+ * Description:
+ *      Struct simply representing a node for our linked list. Each node
+ *      holds a next pointer and an integer value.
+ * 
+ * Public Methods:
+ *                          Node()                               
+ *                          Node(int)
+ * 
+ * Private Methods:
+ *      N/A
+ * 
+ * Usage: 
+ * 
+ *      Node H(int):                        // Create Instance of Node
+ *      Node Y():                           // Create instance of Node
+ *      
+ */
 struct Node {
     int x;
     Node *next;
+    
+    /**
+     * Public : Node
+     * 
+     * Description:
+     *      Node constructor
+     * 
+     * Params:
+     *      N/A
+     * 
+     * Returns:
+     *      A constructed node
+     */
     Node() {
         x = -1;
         next = NULL;
     }
+    
+    
     Node(int n) {
         x = n;
         next = NULL;
     }
 };
 
+
+/**
+ * Huffman
+ * 
+ * Description:
+ *      This class implements a compressions algorithm called Huffman Coding.
+ *      Huffman coding assigns codes to characters such that the length of the 
+ *      code depends on the relative frequency or weight of the corresponding 
+ *      character. Huffman codes are of variable-length, and prefix-free
+ * 
+ * Public Methods:
+ *                          Huffman()                               
+ *      void                BuildFrequencyTable(string filename)
+ *      string              LookupCode(char key)
+ *      void                Analyze()
+ *      map<char, string>   GetCodes()
+ * 
+ * Private Methods:
+ *      void                _BuildLookupTable
+ *      void                _BuildTree
+ *      int                 _maxDepth
+ * 
+ * Usage: 
+ * 
+ *      Huffman H(filename):                        // Create Instance of Huffman
+ *                                                  // and build freq table. 
+ *      H.GetCodes();                               // get map <char,string> of codes
+ * 
+ *                                                  // or
+ *      
+ *      Huffman H;                                  // do seperately
+ *      H.BuildFrequencyTable(filename);            // or use to re-build another file
+ *      H.LookupCode('s')                           // get code for 's' 
+ *      
+ */
 class List {
 private:
     Node *Head;
